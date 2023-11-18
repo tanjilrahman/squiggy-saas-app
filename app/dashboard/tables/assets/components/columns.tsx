19205ec1366dd65cbfa90f2bf8b69de0bd084e60
Tanjil Rahman <tanjil.rahman739@gmail.com>
2023-11-18 19:42:25 +0600

    v2.1

--- app/dashboard/tables/assets/components/columns.tsx
+++ app/dashboard/tables/assets/components/columns.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ColumnDef } from "@tanstack/react-table";
+import { ColumnDef, Row, Table } from "@tanstack/react-table";
 import { categories } from "../data/data";
 import { Asset } from "../data/schema";
 import { DataTableColumnHeader } from "./data-table-column-header";
@@ -26,10 +26,146 @@ import ColumnName from "./column/column-name";
 import ColumnValue from "./column/column-value";
 import ColumnYoy from "./column/column-yoy";
 
+function NameCell<TData>({ row }: { row: Row<TData> }) {
+  const { updateAssetName } = useAssetStore();
+
+  return <ColumnName row={row} updateFunc={updateAssetName} />;
+}
+
+function CategoryCell<TData>({ row }: { row: Row<TData> }) {
+  const { expanded, isEditable } = useAssetExpandedState();
+
+  if (row.getValue("id") === expanded)
+    return (
+      <DataTableCombobox
+        disabled={!isEditable}
+        id={row.getValue("id")}
+        categories={categories}
+        category={row.getValue("category")}
+      />
+    );
+
+  return (
+    <DataTableCombobox
+      disabled={row.getValue("id") !== expanded}
+      id={row.getValue("id")}
+      categories={categories}
+      category={row.getValue("category")}
+    />
+  );
+}
+
+function ValueCell<TData>({ row }: { row: Row<TData> }) {
+  const { updateAssetValue } = useAssetStore();
+  return <ColumnValue row={row} updateFunc={updateAssetValue} />;
+}
+
+function YoyCell<TData>({ row }: { row: Row<TData> }) {
+  const { updateAssetYoy } = useAssetStore();
+  return <ColumnYoy row={row} updateFunc={updateAssetYoy} />;
+}
+
+function DetailsHeader() {
+  const { assets, addAsset } = useAssetStore();
+  const { setIsEditable, setExpanded } = useAssetExpandedState();
+  const newAssetId = (assets[0]?.id ? +assets[0]?.id + 1 : 1).toString();
+
+  const newAsset: Asset = {
+    id: newAssetId,
+    name: "",
+    value: 0,
+    category: "",
+    note: "",
+    yoy: 0,
+    profit: 0,
+    roi: 0,
+    incomes: [],
+    costs: [],
+  };
+  const handleAddAsset = () => {
+    addAsset(newAsset);
+    setExpanded(newAssetId);
+    setIsEditable(true);
+  };
+
+  return (
+    <Button
+      variant="outline"
+      className="flex space-x-1 h-8 px-2 data-[state=open]:bg-muted ml-auto"
+      onClick={handleAddAsset}
+    >
+      <PlusCircle className="h-4 w-4" />
+      <span>Add Asset</span>
+    </Button>
+  );
+}
+
+function DetailsCell<TData>({ row }: { row: Row<TData> }) {
+  const { expanded, isEditable, setIsEditable, setExpanded } =
+    useAssetExpandedState();
+  const { removeAsset } = useAssetStore();
+
+  const handleRemove = () => {
+    removeAsset(row.getValue("id"));
+    setExpanded(null);
+    setIsEditable(false);
+  };
+  return (
+    <div className="flex space-x-2 justify-end ml-auto items-center">
+      <div
+        className={`${
+          expanded !== row.getValue("id") && "opacity-0"
+        } flex space-x-2`}
+      >
+        <Button
+          disabled={expanded !== row.getValue("id")}
+          variant={isEditable ? "ghost" : "outline"}
+          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted mx-auto"
+          onClick={() => setIsEditable(!isEditable)}
+        >
+          <Pencil className="h-4 w-4" />
+          <span className="sr-only">Pencil</span>
+        </Button>
+        <DataTableRemove handleRemove={handleRemove}>
+          <Button
+            disabled={expanded !== row.getValue("id")}
+            variant="outline"
+            className="flex h-8 w-8 p-0 data-[state=open]:bg-muted mx-auto"
+          >
+            <Trash2 className="h-4 w-4" />
+            <span className="sr-only">Trash</span>
+          </Button>
+        </DataTableRemove>
+      </div>
+      <Button
+        variant="ghost"
+        className="flex h-8 w-8 p-0 data-[state=open]:bg-muted ml-auto"
+        onClick={() => {
+          if (expanded === row.getValue("id")) {
+            setExpanded(null);
+            setIsEditable(false);
+          } else {
+            setExpanded(row.getValue("id"));
+            setIsEditable(false);
+          }
+        }}
+      >
+        {expanded === row.getValue("id") ? (
+          <ChevronUp className="h-4 w-8" />
+        ) : (
+          <ChevronDown className="h-4 w-4" />
+        )}
+        <span className="sr-only">Toggle</span>
+      </Button>
+    </div>
+  );
+}
+
 export const columns: ColumnDef<Asset>[] = [
   {
     id: "select",
     header: ({ table }) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
       const { setSelectedAssets } = useSelectedAssetStore();
       useEffect(() => {
         const selectedAssets = table
@@ -77,11 +213,7 @@ export const columns: ColumnDef<Asset>[] = [
         className="translate-x-3"
       />
     ),
-    cell: ({ row }) => {
-      const { updateAssetName } = useAssetStore();
-
-      return <ColumnName row={row} updateFunc={updateAssetName} />;
-    },
+    cell: NameCell,
   },
 
   {
@@ -93,28 +225,7 @@ export const columns: ColumnDef<Asset>[] = [
         className="translate-x-4"
       />
     ),
-    cell: ({ row }) => {
-      const { expanded, isEditable } = useAssetExpandedState();
-
-      if (row.getValue("id") === expanded)
-        return (
-          <DataTableCombobox
-            disabled={!isEditable}
-            id={row.getValue("id")}
-            categories={categories}
-            category={row.getValue("category")}
-          />
-        );
-
-      return (
-        <DataTableCombobox
-          disabled={row.getValue("id") !== expanded}
-          id={row.getValue("id")}
-          categories={categories}
-          category={row.getValue("category")}
-        />
-      );
-    },
+    cell: CategoryCell,
     filterFn: (row, id, value) => {
       return value.includes(row.getValue(id));
     },
@@ -128,10 +239,7 @@ export const columns: ColumnDef<Asset>[] = [
         className="translate-x-3"
       />
     ),
-    cell: ({ row }) => {
-      const { updateAssetValue } = useAssetStore();
-      return <ColumnValue row={row} updateFunc={updateAssetValue} />;
-    },
+    cell: ValueCell,
     filterFn: (row, id, value) => {
       return value.includes(row.getValue(id));
     },
@@ -145,10 +253,7 @@ export const columns: ColumnDef<Asset>[] = [
         className="translate-x-3"
       />
     ),
-    cell: ({ row }) => {
-      const { updateAssetYoy } = useAssetStore();
-      return <ColumnYoy row={row} updateFunc={updateAssetYoy} />;
-    },
+    cell: YoyCell,
     filterFn: (row, id, value) => {
       return value.includes(row.getValue(id));
     },
@@ -189,99 +294,7 @@ export const columns: ColumnDef<Asset>[] = [
   },
   {
     id: "details",
-    header: ({ column }) => {
-      const { assets, addAsset } = useAssetStore();
-      const { setIsEditable, setExpanded } = useAssetExpandedState();
-      const newAssetId = (assets[0]?.id ? +assets[0]?.id + 1 : 1).toString();
-
-      const newAsset: Asset = {
-        id: newAssetId,
-        name: "",
-        value: 0,
-        category: "",
-        note: "",
-        yoy: 0,
-        profit: 0,
-        roi: 0,
-        incomes: [],
-        costs: [],
-      };
-      const handleAddAsset = () => {
-        addAsset(newAsset);
-        setExpanded(newAssetId);
-        setIsEditable(true);
-      };
-
-      return (
-        <Button
-          variant="outline"
-          className="flex space-x-1 h-8 px-2 data-[state=open]:bg-muted ml-auto"
-          onClick={handleAddAsset}
-        >
-          <PlusCircle className="h-4 w-4" />
-          <span>Add Asset</span>
-        </Button>
-      );
-    },
-    cell: ({ row }) => {
-      const { expanded, isEditable, setIsEditable, setExpanded } =
-        useAssetExpandedState();
-      const { removeAsset } = useAssetStore();
-
-      const handleRemove = () => {
-        removeAsset(row.getValue("id"));
-        setExpanded(null);
-        setIsEditable(false);
-      };
-      return (
-        <div className="flex space-x-2 justify-end ml-auto items-center">
-          <div
-            className={`${
-              expanded !== row.getValue("id") && "opacity-0"
-            } flex space-x-2`}
-          >
-            <Button
-              disabled={expanded !== row.getValue("id")}
-              variant={isEditable ? "ghost" : "outline"}
-              className="flex h-8 w-8 p-0 data-[state=open]:bg-muted mx-auto"
-              onClick={() => setIsEditable(!isEditable)}
-            >
-              <Pencil className="h-4 w-4" />
-              <span className="sr-only">Pencil</span>
-            </Button>
-            <DataTableRemove handleRemove={handleRemove}>
-              <Button
-                disabled={expanded !== row.getValue("id")}
-                variant="outline"
-                className="flex h-8 w-8 p-0 data-[state=open]:bg-muted mx-auto"
-              >
-                <Trash2 className="h-4 w-4" />
-                <span className="sr-only">Trash</span>
-              </Button>
-            </DataTableRemove>
-          </div>
-          <Button
-            variant="ghost"
-            className="flex h-8 w-8 p-0 data-[state=open]:bg-muted ml-auto"
-            onClick={() => {
-              if (expanded === row.getValue("id")) {
-                setExpanded(null);
-                setIsEditable(false);
-              } else {
-                setExpanded(row.getValue("id"));
-                setIsEditable(false);
-              }
-            }}
-          >
-            {expanded === row.getValue("id") ? (
-              <ChevronUp className="h-4 w-8" />
-            ) : (
-              <ChevronDown className="h-4 w-4" />
-            )}
-            <span className="sr-only">Toggle</span>
-          </Button>
-        </div>
-      );
-    },
+    header: DetailsHeader,
+    cell: DetailsCell,
   },
 ];
