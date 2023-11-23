04c4f7445a148f9ac2e7e0a3029cd298b53eb36b
Tanjil Rahman <tanjil.rahman739@gmail.com>
2023-11-23 19:35:56 +0600

    v3.5 - asset sync with db

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -40,11 +40,9 @@ export default function SignUpPage() {
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
+                &ldquo;Some information here maybe...&rdquo;
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
+              <footer className="text-sm">Footer...</footer>
             </blockquote>
           </div>
         </div>
