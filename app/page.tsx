8467ad76bec9f89605c60666e150445ae81b4458
Tanjil Rahman <tanjil.rahman739@gmail.com>
2023-12-26 14:39:28 +0600

    v8.8 - plan simplified

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  const { userId } = auth();
+  if (userId) return redirect("/dashboard");
   return (
     <main className="text-center ">
       <h1>Landing page</h1>
